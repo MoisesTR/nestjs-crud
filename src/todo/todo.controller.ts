c6d06f9ee96ecec94b163275e01d8373aa99248b
Moises Trigueros <moisestrigueros@pop-os.localdomain>
2021-08-16 12:07:24 -0600

    initial commit

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from "@nestjs/common";
+import { Todo } from "./todo.interface";
+import { TodoService } from "./todo.service";
+
+@Controller("todo") 
+export class TodoController {
+    constructor(private readonly todoService: TodoService) {
+
+    }
+
+
+    @Get()
+    findAll(): Todo[] {
+        return [];
+    }
+}
