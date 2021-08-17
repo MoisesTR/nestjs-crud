e52e2f2d59b9136d1f0136a05c78c18cb2270e81
MoisesTR <moisestrigueros@hotmail.com>
2021-08-16 22:12:18 -0600

    complete crud functionalities

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,16 +1,39 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { Todo } from "./todo.interface";
 import { TodoService } from "./todo.service";
 
 @Controller("todo") 
 export class TodoController {
-    constructor(private readonly todoService: TodoService) {
+    private readonly logger = new Logger(TodoController.name);
+    constructor(private readonly todoService: TodoService) {}
 
+    @Post()
+    create(@Body() todo: Todo): void {
+        this.logger.log("Handling create request");
+        this.todoService.create(todo);
     }
-
 
     @Get()
     findAll(): Todo[] {
-        return [];
+        this.logger.log("Handling findAll request");
+        return this.todoService.findAll();
+    }
+
+    @Get(":id")
+    findOne(@Param("id", ParseIntPipe) id: number): Todo | null {
+        this.logger.log(`Handling findOne request with id=${id}`);
+        return this.todoService.findOne(id);
+    }
+
+    @Put(":id")
+    update(@Param("id", ParseIntPipe) id: number, @Body() todo: Todo) : void {
+        this.logger.log(`Handling update request with id=${id}`);
+        this.todoService.update(id, todo);
+    }
+
+    @Delete(":id")
+    delete(@Param("id", ParseIntPipe) id: number): void {
+        this.logger.log(`Handling remove request with id=${id}`);
+        this.todoService.remove(id);
     }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,4 +4,28 @@ import { Todo } from "./todo.interface";
 @Injectable()
 export class TodoService {
     private storage: Todo[] = [];
+
+    create(todo: Todo): void {
+        const currentID = Math.max(...this.storage.map( todo => todo.id), 0);
+        const id = currentID + 1;
+        this.storage.push({ ...todo, id});
+    }
+
+    findAll(): Todo[] {
+        return this.storage;
+    }
+
+    findOne(id: number): Todo | null {
+        return this.storage.find( todo => todo.id === id);
+    }
+
+    update(id: number, todo: Todo): void {
+        const index = this.storage.findIndex( todo => todo.id === id);
+        this.storage[index] = {...todo, id};
+    }
+
+    remove(id: number): void {
+        const index = this.storage.findIndex( todo => todo.id === id);
+        this.storage.splice(index, 1);
+    }
 }
